b4a10133a06d381c2ef01cd09ace357709d8f23d
dhruvsingh510 <dhruvsingh510@gmail.com>
2023-10-25 13:29:58 +0300

    adding config server with auto updates

---
+++ config-server/src/main/java/javacourse/microservices/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package javacourse.microservices.configserver;
+
+import com.netflix.discovery.EurekaNamespace;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
